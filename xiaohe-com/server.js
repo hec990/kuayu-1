e1ef026777729ef5dd76c2e54ea20d615ae15ef2
xiaohehe <1296522554@qq.com>
2021-09-18 14:15:47 +0800

    init

---
+++ xiaohe-com/server.js
@@ -0,0 +1,61 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if (!port) {
+    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+    process.exit(1)
+}
+
+var server = http.createServer(function(request, response) {
+    var parsedUrl = url.parse(request.url, true)
+    var pathWithQuery = request.url
+    var queryString = ''
+    if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+    var path = parsedUrl.pathname
+    var query = parsedUrl.query
+    var method = request.method
+
+    /******** 从这里开始看，上面不要看 ************/
+
+    console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+
+    // 访问根目录，响应的内容
+    if (path === '/') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(`
+        <!DOCTYPE html>
+        <head>
+          <link rel="stylesheet" href="/x">
+        </head>
+        <body>
+        <h1>我的第一个HTTP请求</h1>
+        <script src="/y"></script>
+        </body>
+        `)
+        response.end()
+    } else if (path === '/index.html') { // 访问根目录带查询参数  ==> CSS code
+        response.statusCode = 200 // 状态码
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(fs.readFileSync("public/index.html"))
+        response.end()
+    } else if (path === '/xiaohe.js') { // 访问根目录带查询参数  ==> JS code
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+        response.write(fs.readFileSync("public/xiaohe.js"))
+        response.write()
+        response.end()
+    } else {
+        response.statusCode = 404
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(`你输入的路径不存在对应的内容`)
+        response.end()
+    }
+
+    /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
